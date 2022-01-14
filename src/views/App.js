99b1819dafe1f35d0e6178cf9f05a4655f0d3646
nghiant <tuannghianguyen1509@gmail.com>
2022-01-14 20:24:23 +0700

    change smth

--- src/views/App.js
+++ src/views/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bet Barca -1 tonight</p>
+        <p>All-in Barca -1 tonight</p>
         <a
           className="App-link"
           href="https://reactjs.org"
